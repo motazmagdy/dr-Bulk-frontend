8cf226bc4bd285d88374b7ec9c7bbc3e454c68a7
Soha <sohaali710@gmail.com>
2023-08-22 01:19:14 +0300

    Edit Header design

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./Header.css";
-import B from "../../Assets/Png Logo/B-only.png";
+import B from "../../Assets/PngLogo/Yellow.png";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -25,15 +25,13 @@ const Header = () => {
   const handleLogout = () => {
     localStorage.removeItem("Token");
     localStorage.removeItem("Role");
-    navigate("/users/home");
+    navigate("/");
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">  
-        <img src={B} onClick={() => navigate("/users/home")} />
-        {/* <Link className="navbar-brand text-light" to="/">
-          Dr BULK
-        </Link> */}
+    <>
+      <nav className="navbar navbar-expand-lg rounded-0 header-wrapper">
+        <img as={Link} src={B} className="brand-icon" alt="logo icon" onClick={() => navigate("/")} />
         <button
           className="navbar-toggler bg-light header-btn"
           type="button"
@@ -45,88 +43,171 @@ const Header = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-      <div className="container">
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/users/home">
-                  {t("Home")}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/users/plans-pricing">
-                  {t("Plans & Pricing")}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/users/instructors">
-                  {t("Instructors")}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/users/eat-smart">
-                  {t("Eat Smart")}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/users/apparel">
-                  {t("Apparel")}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/users/contactus">
-                  {t("Contact us")}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link language-btn"
-                  value={"ar"}
-                  onClick={changeLanguage}
-                >
-                  {t("ChangeLangauge")}
-                </Link>
-              </li>
-            <div className="user-action">
-        {!isLoggedIn ? (
-              <button className="login-item btn">
-                <Link to="/users/login">
-                  {" "}
-                  <b>{t("Login")}</b>
-                </Link>{" "}
-              </button>
-            ) : null}
-            {!isLoggedIn ? (
-              <button className="signup-item btn">
-                <Link to="/users/signup">
-                  {" "}
-                  <b>{t("Signup")}</b>
-                </Link>{" "}
-              </button>
-            ) : null}
-            {isLoggedIn && role === "admin" ? (
-              <li className="signup-item btn">
-                <Link className="" to="/admin/change-password">
-                  {" "}
-                  {t("Change password")}
-                </Link>{" "}
-              </li>
-            ) : null}
-            {isLoggedIn ? (
-              <li className="signup-item btn">
-                <Link className="logout" onClick={handleLogout}>
-                  {" "}
-                  {t("Logout")}
-                </Link>{" "}
-              </li>
-            ) : null}
-      </div>
-            
-            </ul>
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <div id="navigation">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
+                    {t("Home")}
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/plans-pricing">
+                    {t("Plans & Pricing")}
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/instructors">
+                    {t("Instructors")}
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/eat-smart">
+                    {t("Eat Smart")}
+                  </NavLink>
+                </li>
+                <li className="nav-item has-sub">
+                  <NavLink className="nav-link" to="/apparel">
+                    {t("Apparel")}
+                  </NavLink>
+                  <ul>
+                    <li><a href="blog-default.html" title="Blog">Blog Default</a></li>
+                    <li><a href="blog-single.html" title="Blog Single ">Blog Single</a></li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/contactus">
+                    {t("Contact us")}
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
-      </div>
-      
-    </nav>
+        </div>
+        <div className="lang">
+          <button
+            className="btn btn-dark language-btn"
+            value={"ar"}
+            onClick={changeLanguage}
+          >
+            {t("ChangeLangauge")}
+          </button>
+        </div>
+        <div className="user-action">
+          {!isLoggedIn ? (
+            <button className="login-item btn">
+              <NavLink to="/login">
+                {" "}
+                <b>{t("Login")}</b>
+              </NavLink>{" "}
+            </button>
+          ) : null}
+          {!isLoggedIn ? (
+            <button className="signup-item btn">
+              <NavLink to="/signup">
+                {" "}
+                <b>{t("Signup")}</b>
+              </NavLink>{" "}
+            </button>
+          ) : null}
+          {isLoggedIn && role === "admin" ? (
+            <li className="signup-item btn">
+              <NavLink className="" to="/admin/change-password">
+                {" "}
+                {t("Change password")}
+              </NavLink>{" "}
+            </li>
+          ) : null}
+          {isLoggedIn ? (
+            <li className="signup-item btn">
+              <NavLink className="logout" onClick={handleLogout}>
+                {" "}
+                {t("Logout")}
+              </NavLink>{" "}
+            </li>
+          ) : null}
+        </div>
+      </nav>
+
+      {/* <nav className="navbar navbar-expand-lg bg-dark rounded-0 header-wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-1 col-md-1 col-sm-12 col-xs-12">
+              <Link to="/">
+                <img src={B} className="brand-icon" alt="brand icon" />
+              </Link>
+
+            </div>
+            <div className="col-lg-6 col-md-8 col-sm-10 col-xs-10">
+              <div className="navigation">
+                <div id="navigation">
+                  <ul>
+                    <li className="active"><a href="index.html" title="Home">Home</a></li>
+
+                    <li className="has-sub"><a href="blog-default.html" title="Blog ">Blog</a>
+                      <ul>
+                        <li><a href="blog-default.html" title="Blog">Blog Default</a></li>
+                        <li><a href="blog-single.html" title="Blog Single ">Blog Single</a></li>
+                      </ul>
+                    </li>
+
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 hidden-md hidden-sm hidden-xs">
+              <div className="header-btn"><a href="#" className="btn btn-default">get started</a></div>
+            </div>
+
+            <div className="col-lg-2 lang">
+              <button
+                className="btn language-btn"
+                value={"ar"}
+                onClick={changeLanguage}
+              >
+                {t("ChangeLangauge")}
+              </button>
+            </div>
+
+            <div className="col-lg-2 user-action">
+              {!isLoggedIn ? (
+                <button className="btn login-item">
+                  <NavLink to="/login">
+                    {" "}
+                    <b>{t("Login")}</b>
+                  </NavLink>{" "}
+                </button>
+              ) : null}
+              {!isLoggedIn ? (
+                <button className="btn signup-item">
+                  <NavLink to="/signup">
+                    {" "}
+                    <b>{t("Signup")}</b>
+                  </NavLink>{" "}
+                </button>
+              ) : null}
+              {isLoggedIn && role === "admin" ? (
+                <li className="signup-item btn">
+                  <NavLink className="" to="/admin/change-password">
+                    {" "}
+                    {t("Change password")}
+                  </NavLink>{" "}
+                </li>
+              ) : null}
+              {isLoggedIn ? (
+                <li className="signup-item btn">
+                  <NavLink className="logout" onClick={handleLogout}>
+                    {" "}
+                    {t("Logout")}
+                  </NavLink>{" "}
+                </li>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </nav> */}
+    </>
   );
 };
 
