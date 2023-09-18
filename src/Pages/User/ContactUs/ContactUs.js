1a3077f2e3147f0214e0dd03ae6872e727a49ea7
Soha <sohaali710@gmail.com>
2023-09-18 19:06:08 +0300

    Contact Us

--- src/Pages/User/ContactUs/ContactUs.js
+++ src/Pages/User/ContactUs/ContactUs.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './ContactUs.css'
 
-const ContactUs = ()=> {
+const ContactUs = () => {
     return (
-        <div>
-            <h3>Contact us</h3>
+        <div className="container contact-us">
+            <div className="col-12">
+                <h3 className="footer-title">contact us</h3>
+            </div>
+
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 m-auto">
+                <div className="footer-widget">
+                    <form>
+                        <div className="form-group">
+                            <label className="control-label" htmlFor="name"></label>
+                            <input id="name" type="text" className="form-control" placeholder="Name" />
+                        </div>
+                        <div className="form-group">
+                            <label className="control-label" htmlFor="Email"></label>
+                            <input id="email" type="text" className="form-control" placeholder="Email" />
+                        </div>
+                        <div className="form-group">
+                            <label className="control-label" htmlFor="Phone"></label>
+                            <input id="phone" type="text" className="form-control" placeholder="Phone" />
+                        </div>
+                        <div className="form-group">
+                            <label className="control-label" htmlFor="textarea"></label>
+                            <textarea className="form-control" id="textarea" name="textarea" rows="3" placeholder="textarea"></textarea>
+                        </div>
+                        <button className="btn bulk-dark-btn btn-sm">submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
